e9ac1e37cf8591499925f2691459fdc52a2e9773
Muyinda ROgers <moverr@gmail.com>
2021-03-19 14:08:12 +0300

    List Accounts

--- app/controllers/AccountsController.scala
+++ app/controllers/AccountsController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import db.tables.Account
 import javax.inject.Inject
 import play.api.mvc._
 
@@ -7,10 +8,13 @@ import scala.concurrent.Future
 
 class AccountsController  @Inject()(cc:ControllerComponents)  extends AbstractController(cc){
 
- //todo: list accounts from the service
-  def list(from:Int,limit:Int) = Action.async{
 
-    Future.successful(Ok("???"))
+  val account =   Account("Muyinda Rogers","123456789",None)
+
+ //todo: list accounts from the service
+  def list() = Action.async{
+    val accounts:Seq[Account] = Seq(account)
+    Future.successful(Ok(accounts))
   }
 
 
