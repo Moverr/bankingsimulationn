7e827194c4027ab204f02d67bd37f4004563dd5a
Muyinda ROgers <moverr@gmail.com>
2021-03-19 19:03:45 +0300

    :Debuggigng

---
+++ app/services/TransactionService.scala
@@ -0,0 +1,14 @@
+package services
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class TransactionService @Inject()(accountService: AccountService){
+
+  //todo: debit
+
+  //todo: credit
+
+  //todo: view transaction based on date and time
+
+}
