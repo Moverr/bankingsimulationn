8f19c6aca0c872568f8d4403e781123b327b75e8
Muyinda ROgers <moverr@gmail.com>
2021-03-19 14:42:58 +0300

    Adding Accounts to the routes

--- app/controllers/AccountsController.scala
+++ app/controllers/AccountsController.scala
@@ -3,18 +3,20 @@ package controllers
 import db.tables.Account
 import javax.inject.Inject
 import play.api.mvc._
+import implicits.AccountResponse._
+import play.api.libs.json.Json
 
 import scala.concurrent.Future
 
 class AccountsController  @Inject()(cc:ControllerComponents)  extends AbstractController(cc){
 
 
-  val account =   Account("Muyinda Rogers","123456789",None)
+  val account =   Account("Muyinda Rogers","12345",None)
 
  //todo: list accounts from the service
   def list() = Action.async{
     val accounts:Seq[Account] = Seq(account)
-    Future.successful(Ok(accounts))
+    Future.successful(Ok(Json.toJson(accounts)))
   }
 
 
