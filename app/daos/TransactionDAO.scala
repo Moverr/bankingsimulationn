e6e9cfb4b4ee0e805dac13aab9e33ce956c56e5f
Muyinda ROgers <moverr@gmail.com>
2021-03-20 14:06:04 +0300

    :Debugging

--- app/daos/TransactionDAO.scala
+++ app/daos/TransactionDAO.scala
@@ -3,11 +3,14 @@ package daos
 import akka.http.scaladsl.model.DateTime
 import db.tables.Transaction
 import javax.inject.Singleton
+ import  collection.mutable
 
 @Singleton
 class TransactionDAO {
 
-  var transactions:Seq[Transaction] = Seq[Transaction]()
+//  var transactions:Seq[Transaction] = Seq[Transaction]()
+
+  val transactions =  mutable.Seq[Transaction]()
 
   //todo: Add Transactiioon
   def Create(transaction:Transaction): Transaction ={
@@ -16,7 +19,7 @@ class TransactionDAO {
   }
 
   //todo: Get Transactioons
-  def list(limit:Int,datetime:DateTime): Seq[Transaction] ={
+  def list(limit:Int,datetime:DateTime): mutable.Seq[Transaction] ={
     transactions
   }
 }
