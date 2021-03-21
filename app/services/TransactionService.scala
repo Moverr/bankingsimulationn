28c5728c6ca797c66cb049a8c0f25edd23157e03
Muyinda ROgers <moverr@gmail.com>
2021-03-21 18:31:57 +0300

    Refactor

--- app/services/TransactionService.scala
+++ app/services/TransactionService.scala
@@ -12,6 +12,22 @@ import scala.concurrent.Future
 @Singleton
 class TransactionService @Inject()(  accountService: AccountService,transactionDAO: TransactionDAO){
 
+  //todo: credit
+  def credit(request:TransactionRequest): Future[Transaction] ={
+    //todo: validate account
+    val responsne = accountService.getByAccNumber(request.accNumber)
+    responsne match {
+      case Some(account: Account) =>{
+        //todo: Debit
+        val transaction = Transaction(account.accNumber,request.amount,TransactionType.credit.toString,request.transactionDate.toString("yyyy-MM-d"));
+        transactionDAO.Create(transaction)
+        //todo: Do Reconnciliation
+        Future.successful(transaction)
+      }
+      case None => throw new NullPointerException("Account does not exist")
+    }
+  }
+
   //todo: debit
   def debit(request:TransactionRequest): Future[Transaction] ={
     //todo: validate account
@@ -33,21 +49,7 @@ class TransactionService @Inject()(  accountService: AccountService,transactionD
   def getNumberOfDailyTransactions(accNumber:String,transactionDate:String,transactionType: TransactionType.Value): Int =
     transactionDAO.list(accNumber,transactionDate).filter(x=>x.transactionType==transactionType).length
 
-  //todo: credit
-  def credit(request:TransactionRequest): Future[Transaction] ={
-    //todo: validate account
-    val responsne = accountService.getByAccNumber(request.accNumber)
-    responsne match {
-      case Some(account: Account) =>{
-        //todo: Debit
-        val transaction = Transaction(account.accNumber,request.amount,TransactionType.credit.toString,request.transactionDate.toString("yyyy-MM-d"));
-        transactionDAO.Create(transaction)
-        //todo: Do Reconnciliation
-        Future.successful(transaction)
-      }
-      case None => throw new NullPointerException("Account does not exist")
-    }
-  }
+
 
   //todo: view transaction based on date and time
 
